8285d3199d036c635f7571232e295ef8f674cde2
Stephen Hopkins <grieversguardian@hotmail.com>
2017-07-02 19:25:15 -0400

    Trivia Game Homework

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,22 @@
 
 //initialize variables
-var arr = ["Martin van Buren was the 11th president of the United States.", 
+var arr = ["There are 10 furlongs in a mile.",
+		   "Monopoly is the world's most popular board game.",
+		   "The oldest active parliament in the world is the British Parliament.",
+		   "Martin van Buren was the 11th president of the United States.", 
 		   "The capital of Brazil is Curitiba.",
 		   "The first video game console was the Atari 2600.",
+		   "Michael J. Fox's middle name is John.",
+		   "A person must be 40 years old to run for President of the United States.",
 		   "A bolt of lightning in six times hotter than the sun.", 
 		   "Bald Eagles are so named because the word balde is an old English word meaning white.", 
 		   "You can start a fire with ice.",
-		   "The Lord of the Rings was first published in 1954."];
+		   "The Lord of the Rings was first published in 1954.",
+		   "A dime has 118 ridges around it's edge.",
+		   "A goldfish has a memory span of 3 seconds.",
+		   "Mosquitos have teeth.",
+		   "A group of flamingos is called a flamboyance.",
+		   ];
 
 var trashbin = [];
 var time = 0;
@@ -24,7 +34,7 @@ var clockRunning = false;
 $(document).ready(function() {
 //hiding my div id that holds results until game ends.
 	$("#hideMe").hide();
-	console.log('hidden');
+
 //timer get!  functioning timer counting dwon, showing and hiding elements when the game ends.
 	var timer = function() {
 		countDown--;
@@ -42,22 +52,20 @@ $(document).ready(function() {
 //click functionality for my buttons, checking against the true or false value of player input versus question
     $("button").click(function() {
       var n = arr.indexOf(rand);  
-      	arr.splice($.inArray(removeItem, arr), n);
-      	console.log(arr);
-      	if($(this).val() == "t1" && n >= 3) {
+      	if($(this).val() == "t1" && n >= 8) {
         	right++;
-      	}else if($(this).val() == "t1" && n <=2) {        
+      	}else if($(this).val() == "t1" && n <=7) {        
         	wrong++;
-      	}else if($(this).val() == "f1" && n <= 2) {        
+      	}else if($(this).val() == "f1" && n <= 7) {        
         	right++;
-      	}else if($(this).val() == "f1"  && n >=3) {      
+      	}else if($(this).val() == "f1"  && n >=8) {      
         	wrong++;
       	}
      	 rand = arr[Math.floor(Math.random() * arr.length)];
       		$("#q1").text(rand);
     }); 
 
-   // $("#audio").play();//music file to play music?
+   
     
 timer();
 });
